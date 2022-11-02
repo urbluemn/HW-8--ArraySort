1c0d192a632074f329557491f59d75202143e63a
urbluemn <nyaroshko1404@gmail.com>
2022-11-03 00:34:08 +0300

    Add project files.

---
+++ ArraysSort/Collection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysSort
+{
+    internal class Collection<T>
+    {
+
+            T[] array = Array.Empty<T>();
+        
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+        public int Count { get { return array.Length; } }
+
+        public T GetItem(int value) //Getting item by index
+        {
+            if (value < array.Length)
+                return array[value];
+            else
+                return default(T);
+        }
+
+        public void Add(T value) //Adds 1 item
+        {
+            T[] newArray = new T[array.Length + 1];
+            newArray[newArray.Length - 1] = value;
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        public void Remove(T value) //Removes first occurence of the item 
+        {
+            int index = Array.IndexOf(array, value);
+            T[] newArray = array.Where((e, i) => i != index).ToArray();
+            array = newArray;
+        }
+
+    }
+}
---
+++ ArraysSort/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите обобщенный класс, который может хранить в массиве объекты любого типа. Кроме того, данный класс должен иметь методы для добавления данных в массив,
+//удаления из массива, получения элемента из массива по индексу и метод, возвращающий длину массива.
+//Отнаследуйте от этого класса экземпляр класса, принимающий в качестве параметра int. Реализуйте операторы сложения, сравнения, а так же индексатор.
+
+//class Collection<T>
+
+//    // добавление данных
+//    public void Add
+//    // удаление данных - удаляем первое вхождение элемента при его наличии
+//    public void Remove
+//    public ??? GetItem
+//    public int Count
+
+//class Collection<int>
+//+ == != this
+
+using ArraysSort;
+
+Collection<int> collection = new Collection<int>();
+
+collection.Add(5);
+collection.Add(2);
+collection.Add(3);
+collection.Add(7);
+collection.Add(9);
+Console.WriteLine(collection.Count);
+collection.Remove(2);
+Console.WriteLine(collection.GetItem(3));
